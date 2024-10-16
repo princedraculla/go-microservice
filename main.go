528a8978ce33d0f4be87da5f16f04928ab92ad43
princedraculla <amirtorkashvand56@gmail.com>
2024-10-16 19:48:59 +0330

    server init

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("somting happend: ", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
 }
