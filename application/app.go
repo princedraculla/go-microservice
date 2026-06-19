a30bd1ab3377f28c7a8b89e903895fb50bd30b07
princedraculla <amirtorkashvand56@gmail.com>
2026-06-20 02:01:48 +0330

    redis connection

--- application/app.go
+++ application/app.go
@@ -2,16 +2,21 @@ package application
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	return &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 }
 
@@ -21,8 +26,14 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
+	if err := a.rdb.Ping(ctx).Err(); err != nil {
+		return fmt.Errorf("faild to connect redis: %w", err)
+	}
+
+	fmt.Println("Starting Server...")
+
 	if err := server.ListenAndServe(); err != nil {
-		return err
+		return fmt.Errorf("faild to start http server: %w", err)
 	}
 
 	return nil
