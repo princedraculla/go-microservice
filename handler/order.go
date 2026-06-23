500ee8abecc68d7b141b6c91df990a7e1a2a9b05
princedraculla <amirtorkashvand56@gmail.com>
2026-06-23 12:04:26 +0330

    create method added

--- handler/order.go
+++ handler/order.go
@@ -1,14 +1,59 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/princedraculla/go-microservice/model"
+	repo "github.com/princedraculla/go-microservice/repository/order"
 )
 
-type Order struct{}
+type Order struct {
+	Repo *repo.RedisRepo
+}
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create method")
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItem   []model.LineItem `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItem,
+		CreatedAt:  &now,
+	}
+
+	err := o.Repo.Insert(r.Context(), order)
+
+	if err != nil {
+		fmt.Println("failed to Insert order:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(order)
+
+	if err != nil {
+		fmt.Println("fialed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
